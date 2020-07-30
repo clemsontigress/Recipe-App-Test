87b7a20117f65ab1939f7c6d203292a0f814e427
clemson tigress <anreeves83@gmail.com>
2020-07-29 22:12:32 -0400

    added card layout for favorites page

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -2,8 +2,51 @@ import React from 'react';
 
 const Favorites = () => {
   return (
-    <div>
-      <h1>Favorites Page</h1>
+    <div className='container'>
+      <div className='ui link cards'>
+        <div className='card'>
+          <div class='image'>
+            <img src='/images/foodFive.jpg'></img>
+          </div>
+          <div className='content'>
+            <div className='header'>Recipe Title</div>
+
+            <div className='description'>Recipe Ingredients</div>
+          </div>
+          <div className='extra content'>
+            <span className='right floated'>View Video</span>
+            <span>View Recipe</span>
+          </div>
+        </div>
+        <div className='card'>
+          <div class='image'>
+            <img src='/images/foodFive.jpg'></img>
+          </div>
+          <div className='content'>
+            <div className='header'>Recipe Title</div>
+
+            <div className='description'>Recipe Ingredients</div>
+          </div>
+          <div className='extra content'>
+            <span className='right floated'>View Video</span>
+            <span>View Recipe</span>
+          </div>
+        </div>
+        <div className='card'>
+          <div class='image'>
+            <img src='/images/foodFive.jpg'></img>
+          </div>
+          <div className='content'>
+            <div className='header'>Recipe Title</div>
+
+            <div className='description'>Recipe Ingredients</div>
+          </div>
+          <div className='extra content'>
+            <span className='right floated'>View Video</span>
+            <span>View Recipe</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
